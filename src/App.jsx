46c8cb0f63356ea2320f6d44ad5186482fa8bbaa
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-29 17:22:12 +0700

    Add header and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+
 function App() {
-  return <h1>Where's Waldo</h1>;
+  return (
+    <>
+      <Header />
+      <main className="w-full max-w-screen-xl mx-auto">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
