2f4c80a2d3b85d8aaf67e7d6b8cfffbaf5785514
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-03 00:00:23 +0700

    Change font size on h1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="w-full max-w-screen-xl flex mx-auto py-5 mb-5">
+    <header className="w-full max-w-screen-xl flex mx-auto py-5 mb-5 items-center">
       <div className="mr-auto">
-        <h1 className="text-xl font-semibold">Where's Waldo</h1>
+        <h1 className="text-2xl font-bold">Where's Waldo</h1>
       </div>
       <nav className="flex gap-7 font-medium">
         <Link>Home</Link>
