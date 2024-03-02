c33248f61d243dea88252a1cea2c430d7ea06da8
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-02 18:54:44 +0700

    Change font size and weight

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ const Header = () => {
   return (
     <header className="w-full max-w-screen-xl flex mx-auto py-5 mb-5">
       <div className="mr-auto">
-        <h1>Where's Waldo</h1>
+        <h1 className="text-xl font-semibold">Where's Waldo</h1>
       </div>
-      <nav className="flex gap-7">
+      <nav className="flex gap-7 font-medium">
         <Link>Home</Link>
         <Link>Leaderboard</Link>
       </nav>
