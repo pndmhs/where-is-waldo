be774f7cba3c5dfc9beb6c09c044a2036b61c239
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-02 23:38:12 +0700

    Add thumbnail image render

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
+import beachThumbnail from "../assets/beach_sm.jpg";
+import skiSlopesThumbnail from "../assets/ski-slopes_sm.jpg";
+import trackAndFieldThumbnail from "../assets/track-and-field_sm.jpg";
+
 const sampleData = [
   {
     name: "Beach",
+    imageUrl: beachThumbnail,
   },
   {
     name: "Track and Field",
+    imageUrl: trackAndFieldThumbnail,
   },
   {
     name: "Ski Slopes",
+    imageUrl: skiSlopesThumbnail,
   },
 ];
 
@@ -18,8 +25,10 @@ const Home = () => {
       </h2>
       <div className="flex w-full justify-between">
         {sampleData.map((data) => (
-          <div className="flex flex-col gap-3 items-center">
-            <div className="w-96 h-64 bg-neutral-400"></div>
+          <div className="flex flex-col gap-3 items-center cursor-pointer">
+            <div className="w-96 h-auto">
+              <img src={data.imageUrl} alt={data.name} />
+            </div>
             <h2>{data.name}</h2>
           </div>
         ))}
