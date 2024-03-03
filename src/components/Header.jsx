eba6ae10293fcba9009977e4d3353a76d1fd98d3
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-03 08:51:08 +0700

    Change navigation layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="w-full max-w-screen-xl flex mx-auto py-5 mb-5 items-center">
       <div className="mr-auto">
-        <h1 className="text-2xl font-bold">Where's Waldo</h1>
+        <Link to="/" className="text-2xl font-bold">
+          Where's Waldo
+        </Link>
       </div>
-      <nav className="flex gap-7 font-medium">
-        <Link>Home</Link>
-        <Link>Leaderboard</Link>
+      <nav>
+        <ul className="flex gap-7 font-medium">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink>Leaderboard</NavLink>
+          </li>
+        </ul>
       </nav>
     </header>
   );
