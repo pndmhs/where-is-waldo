7a0ced98a5e0989749eea58acf1ff0455d88022d
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-03 09:36:47 +0700

    Add a div to wrap all elements

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <>
+    <div className="bg-[#2A2B2A] text-white min-h-screen w-full">
       <Header />
       <main className="w-full max-w-screen-xl mx-auto">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </main>
-    </>
+    </div>
   );
 }
 
