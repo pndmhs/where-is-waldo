f224430449dcce9b01b6325396c1e005d38f9270
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-03 09:44:25 +0700

    Add Inter font import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
 
 function App() {
   return (
-    <div className="bg-[#2A2B2A] text-white min-h-screen w-full">
+    <div className="bg-[#2A2B2A] text-white min-h-screen w-full font-inter">
       <Header />
-      <main className="w-full max-w-screen-xl mx-auto">
+      <main className="w-full max-w-screen-xl mx-auto mt-12">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
