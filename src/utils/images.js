eb9f1d4e3e3337a741f84932e9a3c9628ac314c1
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-04 10:50:54 +0700

    Create image file to store images data

---
+++ src/utils/images.js
@@ -0,0 +1,30 @@
+import beachThumbnail from "../assets/thumbnail/beach_sm.jpg";
+import skiSlopesThumbnail from "../assets/thumbnail/ski-slopes_sm.jpg";
+import trackAndFieldThumbnail from "../assets/thumbnail/track-and-field_sm.jpg";
+
+import beachImage from "../assets/game-images/beach.webp";
+import skiSlopesImage from "../assets/game-images/ski-slopes.webp";
+import trackAndFieldImage from "../assets/game-images/track-and-field.webp";
+
+const gameImages = [
+  {
+    name: "Beach",
+    route: "beach",
+    image: beachImage,
+    thumbnail: beachThumbnail,
+  },
+  {
+    name: "Ski Slopes",
+    route: "ski-slopes",
+    image: skiSlopesImage,
+    thumbnail: skiSlopesThumbnail,
+  },
+  {
+    name: "Track and Field",
+    route: "track-and-field",
+    image: trackAndFieldImage,
+    thumbnail: trackAndFieldThumbnail,
+  },
+];
+
+export default gameImages;
