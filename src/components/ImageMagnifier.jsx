8d29adb79ae038eece69c1d5a7d130dc81958456
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-04 17:03:27 +0700

    Create image magnifier component

---
+++ src/components/ImageMagnifier.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+const ImageMagnifier = ({
+  src,
+  handleClick,
+  magnifierHeight = 100,
+  magnifieWidth = 100,
+  zoomLevel = 2,
+}) => {
+  const [[x, y], setXY] = useState([0, 0]);
+  const [[imgWidth, imgHeight], setSize] = useState([0, 0]);
+  const [showMagnifier, setShowMagnifier] = useState(false);
+
+  return (
+    <div className="relative" onClick={handleClick}>
+      <img
+        src={src}
+        className="w-full"
+        onMouseEnter={(e) => {
+          // update image size and turn-on magnifier
+          const elem = e.currentTarget;
+          const { width, height } = elem.getBoundingClientRect();
+          setSize([width, height]);
+          setShowMagnifier(true);
+        }}
+        onMouseMove={(e) => {
+          // update cursor position
+          const elem = e.currentTarget;
+          const { top, left } = elem.getBoundingClientRect();
+
+          // calculate cursor position on the image
+          const x = e.pageX - left - window.scrollX;
+          const y = e.pageY - top - window.scrollY;
+          setXY([x, y]);
+        }}
+        onMouseLeave={() => {
+          // close magnifier
+          setShowMagnifier(false);
+        }}
+        alt={"img"}
+      />
+
+      {showMagnifier && (
+        <div
+          className="absolute pointer-events-none border-2 border-neutral-400 opacity-100 bg-[#2A2B2A]"
+          style={{
+            // set size of magnifier
+            height: `${magnifierHeight}px`,
+            width: `${magnifieWidth}px`,
+            // move element center to cursor pos
+            top: `${y - magnifierHeight / 2}px`,
+            left: `${x - magnifieWidth / 2}px`,
+            backgroundImage: `url('${src}')`,
+            backgroundRepeat: "no-repeat",
+
+            //calculate zoomed image size
+            backgroundSize: `${imgWidth * zoomLevel}px ${
+              imgHeight * zoomLevel
+            }px`,
+
+            //calculate position of zoomed image.
+            backgroundPositionX: `${-x * zoomLevel + magnifieWidth / 2}px`,
+            backgroundPositionY: `${-y * zoomLevel + magnifierHeight / 2}px`,
+          }}
+        ></div>
+      )}
+    </div>
+  );
+};
+
+export default ImageMagnifier;
