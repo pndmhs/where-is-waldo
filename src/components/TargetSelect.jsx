590b73206968cfe0d1f26a333fe4d926be2c4c35
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-07 07:54:06 +0700

    Add TargetDropdown component

--- src/components/TargetSelect.jsx
+++ src/components/TargetSelect.jsx
@@ -1,12 +1,17 @@
+import TargetDropdown from "./TargetDropdown";
+
 const TargetSelect = ({ coordinate }) => {
   return (
-    <div
-      className={`absolute w-[50px] h-[50px] rounded-full border-4 border-red-500`}
-      style={{
-        top: `${coordinate.y - 50 / 2}px`,
-        left: `${coordinate.x - 50 / 2}px`,
-      }}
-    ></div>
+    <>
+      <div
+        className={`absolute w-[50px] h-[50px] rounded-full border-4 border-red-500`}
+        style={{
+          top: `${coordinate.y - 50 / 2}px`,
+          left: `${coordinate.x - 50 / 2}px`,
+        }}
+      ></div>
+      <TargetDropdown coordinate={coordinate} />
+    </>
   );
 };
 
