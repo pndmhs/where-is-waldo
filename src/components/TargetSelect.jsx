4baf4ef0564b81068bbffb035a3f5c282079c31b
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-07 07:13:00 +0700

    Create TargetSelect component

---
+++ src/components/TargetSelect.jsx
@@ -0,0 +1,13 @@
+const TargetSelect = ({ coordinate }) => {
+  return (
+    <div
+      className={`absolute w-[50px] h-[50px] rounded-full border-4 border-red-500`}
+      style={{
+        top: `${coordinate.y - 50 / 2}px`,
+        left: `${coordinate.x - 50 / 2}px`,
+      }}
+    ></div>
+  );
+};
+
+export default TargetSelect;
