928b4e28dd37e2cbac55b6fac673fcac509636ec
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-11 22:58:10 +0700

    Add leaderboard page route

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
 import "@fontsource/inter/800.css";
 import Game from "./pages/Game";
+import Leaderboard from "./pages/Leaderboard";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/game/:name" element={<Game />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
         </Routes>
       </main>
     </div>
