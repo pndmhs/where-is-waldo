04d72c4ebeccf685ca643a4d282d0d938dba6c51
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-13 16:35:55 +0700

    Create CorrectTarget component

---
+++ src/components/CorrectTarget.jsx
@@ -0,0 +1,15 @@
+const CorrectTarget = ({ coordinate }) => {
+  console.log(coordinate);
+
+  return (
+    <div
+      className={`absolute w-[50px] h-[50px] rounded-full border-4 border-green-600`}
+      style={{
+        top: `${coordinate.y - 50 / 2}px`,
+        left: `${coordinate.x - 50 / 2}px`,
+      }}
+    ></div>
+  );
+};
+
+export default CorrectTarget;
