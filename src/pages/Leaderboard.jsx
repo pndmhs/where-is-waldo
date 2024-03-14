28f966fe3318b925b0b8decea603f91b84fcb4b5
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-14 22:43:52 +0700

    Change sample data with data fetched from API

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,37 +1,33 @@
 import { useState } from "react";
 import ScoreTable from "../components/ScoreTable";
 import LeaderboardButtons from "../components/LeaderboardButtons";
-
-const sampleScores = [
-  {
-    username: "palmer20",
-    time_score: "35s",
-  },
-  {
-    username: "gusto27",
-    time_score: "40s",
-  },
-  {
-    username: "enzo5",
-    time_score: "45s",
-  },
-];
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const Leaderboard = () => {
-  const [activeButton, setActiveButton] = useState(0);
+  const [activeGame, setActiveGame] = useState("beach");
 
-  const handleClick = (buttonIndex) => {
-    setActiveButton(buttonIndex);
+  const scoreQuery = useQuery({
+    queryKey: ["score", activeGame],
+    queryFn: async () => {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/scores/${activeGame}`
+      );
+      return response.data;
+    },
+  });
+
+  const handleClick = (game) => {
+    setActiveGame(game);
   };
 
   return (
     <div className="w-full flex flex-col items-center gap-5">
       <h1 className="text-4xl">Leaderboard</h1>
-      <LeaderboardButtons
-        activeButton={activeButton}
-        handleClick={handleClick}
-      />
-      <ScoreTable data={sampleScores} />
+      <LeaderboardButtons activeGame={activeGame} handleClick={handleClick} />
+      {scoreQuery.isLoading && <p>Loading...</p>}
+      {scoreQuery.isError && <p>Error: {scoreQuery.error}</p>}
+      {scoreQuery.data && <ScoreTable data={scoreQuery.data} />}
     </div>
   );
 };
