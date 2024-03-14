d0b44112db3f14af66c77fbb8d4fb4f6a5597a2d
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-14 22:45:39 +0700

    Change time_score to score and add formatDate function

--- src/components/ScoreTable.jsx
+++ src/components/ScoreTable.jsx
@@ -1,6 +1,12 @@
+import { DateTime } from "luxon";
+
 const ScoreTable = ({ data }) => {
+  const formatDate = (date) => {
+    return DateTime.fromISO(date).toLocaleString(DateTime.DATE_MED);
+  };
+
   return (
-    <table className="rounded-lg table-auto w-full max-w-[750px] bg-[#313531] border-spacing-0 border-separate overflow-hidden">
+    <table className="rounded-lg table-fixed w-full max-w-[750px] bg-[#313531] border-spacing-0 border-separate overflow-hidden">
       <thead className="text-left bg-[#3e413e]">
         <tr>
           <th className="py-4 px-6">Place</th>
@@ -14,8 +20,8 @@ const ScoreTable = ({ data }) => {
           <tr key={index}>
             <td className="py-4 px-6">{index + 1}</td>
             <td className="py-4 px-6">{item.username}</td>
-            <td className="py-4 px-6">{item.time_score}</td>
-            <td className="py-4 px-6">-</td>
+            <td className="py-4 px-6">{item.score}s</td>
+            <td className="py-4 px-6">{formatDate(item.date)}</td>
           </tr>
         ))}
       </tbody>
