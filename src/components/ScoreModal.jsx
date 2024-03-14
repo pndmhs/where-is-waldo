e0f531e07f90ca9917d4725a2d9e00e8d056a8a2
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-14 11:37:20 +0700

    Add function to post score data to API

--- src/components/ScoreModal.jsx
+++ src/components/ScoreModal.jsx
@@ -1,7 +1,32 @@
+import axios from "axios";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const ScoreModal = ({ time_score }) => {
+const ScoreModal = ({ time_score, game_name }) => {
+  const [username, setUsername] = useState("");
+
   const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/scores`,
+        {
+          username,
+          score: time_score,
+          game_name,
+        }
+      );
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="fixed w-full h-full flex justify-center items-center bg-black/50 top-0 left-0">
@@ -10,13 +35,18 @@ const ScoreModal = ({ time_score }) => {
           You finished the game in {time_score.toFixed(3)}s!
         </h1>
         <p className="mb-5">Submit your score to the Leaderboard</p>
-        <form className="flex flex-col gap-3">
+        <form
+          method="post"
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-3"
+        >
           <label htmlFor="username">Username</label>
           <input
             type="text"
             name="username"
             id="username"
             className="p-2 rounded-md bg-[#373937] border border-neutral-400 outline-none focus:border-white"
+            onChange={handleChange}
           />
           <div className="flex gap-2">
             <button
