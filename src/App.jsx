8648326fb86ece779f07b6addb55b9366a4c2661
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-15 18:06:44 +0700

    Add NotFound page route

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,18 @@ import "@fontsource/inter/700.css";
 import "@fontsource/inter/800.css";
 import Game from "./pages/Game";
 import Leaderboard from "./pages/Leaderboard";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="bg-[#2A2B2A] text-white min-h-screen w-full font-inter overflow-x-hidden">
+    <div className="bg-[#2A2B2A] text-white min-h-screen w-full font-inter overflow-x-hidden flex flex-col">
       <Header />
-      <main className="w-full max-w-screen-xl min-h-full mx-auto py-12 px-5 xl:px-0">
+      <main className="w-full max-w-screen-xl min-h-full mx-auto py-12 px-5 xl:px-0 grow flex flex-col">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/game/:name" element={<Game />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
     </div>
