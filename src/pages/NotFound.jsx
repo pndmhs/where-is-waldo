51ec751dfd538991b92a139bffcb256581825b4a
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-15 18:06:23 +0700

    Create NotFound page component

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+  return (
+    <div className="w-full h-full text-center my-auto">
+      <h1 className="text-9xl font-semibold mb-4">404</h1>
+      <p className="text-3xl">
+        Oops! The page you are looking for doesn't exist
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
